938e8c154df3dd67809aa1b1aac306099eae4869
Hansana Sandeepa <151901922+HansSandeepa@users.noreply.github.com>
2026-04-16 14:09:03 +0530

    readded created user models

---
+++ src/main/java/acadflow/models/Student.java
@@ -0,0 +1,73 @@
+package acadflow.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends User {
+    private String studentId;
+    private int batchYear;
+    private boolean isRepeat;
+    private boolean isBatchMissed;
+    private double gpa;
+
+
+    public Student() {
+        this.role = "student";
+
+    }
+
+    public Student(int userId, String username, String fullName, String studentId) {
+        super(userId, username, fullName, "student");
+        this.studentId = studentId;
+    }
+
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getBatchYear() {
+        return batchYear;
+    }
+
+    public void setBatchYear(int batchYear) {
+        this.batchYear = batchYear;
+    }
+
+    public boolean isRepeat() {
+        return isRepeat;
+    }
+
+    public void setRepeat(boolean repeat) {
+        isRepeat = repeat;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+
+    @Override
+    public boolean canManageUsers() {
+        return false;
+    }
+
+    @Override
+    public boolean canUploadMarks() {
+        return false;
+    }
+
+    @Override
+    public boolean canManageAttendance() {
+        return false;
+    }
+
+}
